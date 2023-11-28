e161717c3202e79c7d0bbbf3da36c356ca0cbf98
mylhyz <mylhyz@gmail.com>
2023-11-28 19:37:22 +0800

    添加导出进度

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const process = require("process");
 const { Client, NoteStore } = require("evernote");
+const fs = require("fs");
 
 const runMain = async () => {
   const client = new Client({
@@ -11,17 +12,18 @@ const runMain = async () => {
   const notebooks = await noteStore.listNotebooks();
   const notebookDataArray = [];
   for (let notebook of notebooks) {
-    // 保存笔记本数据
-    const noteDataArray = [];
-    
     let filter = new NoteStore.NoteFilter({
       notebookGuid: notebook.guid,
     });
-    const metaList = await noteStore.findNotesMetadata(filter, 0, 200, {
+    const metaList = await noteStore.findNotesMetadata(filter, 0, 300, {
       includeTitle: true,
     });
+    console.log(`exporting notebooks... [${notebook.name}]`);
+    console.log(
+      `exporting ${metaList.notes.length} note for [${notebook.name}]`
+    );
+    const noteDataArray = [];
     for (let meta of metaList.notes) {
-      console.log(meta.guid, meta.title);
       const note = await noteStore.getNoteWithResultSpec(meta.guid, {
         includeContent: true,
         includeResourcesData: true,
@@ -32,13 +34,30 @@ const runMain = async () => {
         includeResourceAppDataValues: true,
         includeAccountLimits: true,
       });
-      console.log(note);
+      fs.writeFileSync(
+        `exported/note_${meta.guid}.json`,
+        JSON.stringify(note, null, 2),
+        { encoding: "utf-8" }
+      );
+      noteDataArray.push({
+        guid: meta.guid,
+        name: meta.name,
+      });
+      console.log(
+        `exported ${meta.name} for [${notebook.name}]`
+      );
     }
     notebookDataArray.push({
       guid: notebook.guid,
       name: notebook.name,
+      array: noteDataArray,
     });
   }
+  fs.writeFileSync(
+    "exported/notebooks.json",
+    JSON.stringify(notebookDataArray, null, 2),
+    { encoding: "utf-8" }
+  );
 };
 
 runMain();
