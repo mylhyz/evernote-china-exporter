ed0cdfff2293c82ac2343ab6d93e677892d00a0f
mylhyz <mylhyz@gmail.com>
2023-11-29 21:28:23 +0800

    使用mustache生成html模板

--- converter/export_to_html.js
+++ converter/export_to_html.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const flow = require("xml-flow");
 const { Readable } = require("stream");
+const Mustache = require("mustache");
 
 const EXPORTED_FOLDER = ".exported-html-cache";
 
@@ -15,8 +16,11 @@ const mkdirSafe = (dir) => {
   }
 };
 
+const generate = (data) => {
+  return "<div><div/>";
+};
+
 async function onNotebooks(context) {
-  context._md_map = {};
   context._html_map = {};
   //创建所有文件夹
   const root = path.join(__dirname, EXPORTED_FOLDER);
@@ -26,9 +30,9 @@ async function onNotebooks(context) {
     mkdirSafe(folder);
     for (let note of notebook.array) {
       const name = note.name.replaceAll("/", "-");
-      const file = path.join(folder, `${name}.md`);
+      const file = path.join(folder, `${name}.html`);
       //给每一个创建笔记文件创建guid关联
-      context._md_map[note.guid] = file;
+      context._html_map[note.guid] = file;
     }
   }
 }
@@ -40,10 +44,20 @@ async function convert(note, file) {
     };
     const stream = Readable.from([note.content]);
     const xml = flow(stream);
-    xml.on('tag:en-note',(data)=>{
-      console.log(data);
-    })
+    const lines = [];
+    xml.on("tag:div", (data) => {
+      lines.push(generate(data));
+    });
     xml.on("end", () => {
+      //组装html文件
+      const template = fs.readFileSync("templates/index.html", {
+        encoding: "utf-8",
+      });
+      const result = Mustache.render(template, {
+        note_title: note.name,
+        note_bodys: lines,
+      });
+      fs.writeFileSync(file, result, { encoding: "utf-8" });
       resolve();
     });
     xml.on("error", logAndReject);
@@ -52,8 +66,13 @@ async function convert(note, file) {
 }
 
 async function onNotes(context) {
+  let count = 1;
   for (let note of context.notes) {
-    await convert(note, context._md_map[note.guid]);
+    if (count > 1) {
+      break;
+    }
+    count = count + 1;
+    await convert(note, context._html_map[note.guid]);
   }
 }
 
