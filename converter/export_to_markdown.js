893d4ae293a9fb32ddb638364300ea501c6581ee
mylhyz <mylhyz@gmail.com>
2023-12-05 21:14:11 +0800

    完成html转换方案

--- converter/export_to_markdown.js
+++ converter/export_to_markdown.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const path = require("path");
-var converter = require("xml-js");
+const crypto = require("crypto");
+const converter = require("xml-js");
 const Mustache = require("mustache");
 
 const EXPORTED_FOLDER = ".exported-markdown-cache";
+const EXPORTED_MEDIA_FOLDER = "media";
 
 const mkdirSafe = (dir) => {
   if (fs.existsSync(dir)) {
@@ -13,6 +15,10 @@ const mkdirSafe = (dir) => {
   } else {
     fs.mkdirSync(dir);
   }
+};
+
+const getMediaSuffix = (mine) => {
+  return mine.slice(mine.indexOf("/") + 1);
 };
 
 const genAttrs = (attrs) => {
@@ -64,8 +70,7 @@ const gen = (data) => {
       data.name == "dl" ||
       data.name == "dt" ||
       data.name == "dd" ||
-      data.name == "blockquote" ||
-      data.name == "en-media"
+      data.name == "blockquote"
     ) {
       // 处理 attributes
       let attr_str = genAttrs(data.attributes);
@@ -86,9 +91,19 @@ const gen = (data) => {
     } else if (data.name == "br") {
       return "<br/>";
     } else if (data.name == "en-todo") {
-      const checked =
-        data.attributes && data.attributes["checked"] ? " checked" : "";
+      let checked = "";
+      if (data.attributes && data.attributes["checked"]) {
+        checked = checked;
+      }
       return `<input type="checkbox"${checked}/>`;
+    } else if (data.name == "en-media") {
+      const suffix = getMediaSuffix(data.attributes.type);
+      const hash = data.attributes.hash;
+      delete data.attributes.type;
+      delete data.attributes.hash;
+      data.attributes.src = `${EXPORTED_MEDIA_FOLDER}/${hash}.${suffix}`;
+      let attr_str = genAttrs(data.attributes);
+      return `<img${attr_str}></img>`;
     } else {
       throw Error(`name ${data.name} ???`);
     }
@@ -113,22 +128,38 @@ const generate = (data) => {
 
 async function onNotebooks(context) {
   context._html_map = {};
+  context._media_map = {};
   //创建所有文件夹
   const root = path.join(__dirname, EXPORTED_FOLDER);
   mkdirSafe(root);
   for (let notebook of context.notebooks) {
     const folder = path.join(root, notebook.name);
     mkdirSafe(folder);
+    const mediaFolder = path.join(folder, EXPORTED_MEDIA_FOLDER);
     for (let note of notebook.array) {
       const name = note.name.replaceAll("/", "_");
       const file = path.join(folder, `${name}.html`);
       //给每一个创建笔记文件创建guid关联
       context._html_map[note.guid] = file;
+      context._media_map[note.guid] = mediaFolder;
     }
   }
 }
 
-async function convert(note, file) {
+async function convert(note, file, media) {
+  // 处理media文件
+  if (note.resources && Array.isArray(note.resources)) {
+    for (let resource of note.resources) {
+      const mimeType = resource.mime;
+      const suffix = getMediaSuffix(mimeType);
+      const bf = Buffer.from(resource.data.body.data);
+      const hashV = crypto.createHash("md5").update(bf).digest("hex");
+      mkdirSafe(media);
+      fs.writeFileSync(`${media}/${hashV}.${suffix}`, bf);
+    }
+  }
+
+  // 处理笔记本体
   const obj = converter.xml2js(note.content);
   const note_bodys = generate(obj);
   if (note_bodys.length == 0) {
@@ -149,7 +180,11 @@ async function convert(note, file) {
 
 async function onNotes(context) {
   for (let note of context.notes) {
-    await convert(note, context._html_map[note.guid]);
+    await convert(
+      note,
+      context._html_map[note.guid],
+      context._media_map[note.guid]
+    );
   }
 }
 
